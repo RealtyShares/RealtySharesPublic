ea1c9af85485fd0afc2fb1d647cd80cf6731ec51
Horace R Gonzalez <horace.r.gonzalez@gmail.com>
2015-10-08 16:01:08 -0700

    Initial commit

--- DynamoDB/StandaloneFSharp/Program.fs
+++ DynamoDB/StandaloneFSharp/Program.fs
@@ -1,102 +1,61 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-
-
-module DynamoDb =
-    open Amazon.DynamoDBv2
-    open Amazon.DynamoDBv2.DocumentModel
-    open Amazon.DynamoDBv2.Model
-    open Amazon.Runtime
-
-    let temp = 1
-    let awsAccessKey = "AKIAIKBBH76QPNGGD25Q"
-    let awsSecretKey = "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"
-
-    let bclList (lst:'a list) = new System.Collections.Generic.List<'a>(lst)
-    let attr = ScalarAttributeType
-    let keyType = KeyType
-
-    //let credentials = new EnvironmentVariablesAWSCredentials()
-    let client = new AmazonDynamoDBClient(awsAccessKey, awsSecretKey, Amazon.RegionEndpoint.USWest1)
-
-    let waitForStatus tableName status = 
-        let rec loop attempt maxretry delay (prevStatus:TableStatus option) = async {
-            if attempt > maxretry then
-                return prevStatus
-            else
-                do! Async.Sleep delay
-                let res = client.DescribeTable(DescribeTableRequest(TableName = tableName))
-                let currStatus = res.Table.TableStatus
-                if res.Table.TableStatus = status then
-                    return Some res.Table.TableStatus
-                else
-                    return! loop (attempt + 1) maxretry delay (Some currStatus)
-        }
-        loop 0 200 50 None
-    
-    let createTable tableName = 
-        client.CreateTableAsync(
-            CreateTableRequest(TableName = tableName,
-                               KeySchema = bclList [
-                                    KeySchemaElement(AttributeName="H", KeyType= keyType "HASH")
-                                    KeySchemaElement(AttributeName="K", KeyType= keyType "RANGE")],
-                               AttributeDefinitions = bclList [
-                                    AttributeDefinition(AttributeName="H", AttributeType = attr "N")
-                                    AttributeDefinition(AttributeName="K", AttributeType = attr "N")],
-                               ProvisionedThroughput = 
-                                    ProvisionedThroughput(ReadCapacityUnits= int64 5, WriteCapacityUnits = int64 5)))
-        |> Async.AwaitTask
-//
-//            return! Async.AwaitTask res
-//            if res.HttpStatusCode = System.Net.HttpStatusCode.OK then
-//                return! waitForStatus tableName TableStatus.ACTIVE
-//            else
-//                return None
-//        with
-//        | :? ResourceInUseException -> 
-//            printfn "Table already exists."
-//            return None
-//        | _ -> 
-//            return None
-//    }
-             
-    let deleteTable tableName = 
-        client.DeleteTableAsync(DeleteTableRequest(tableName))
-        |> Async.AwaitTask
-               
-
-open Amazon.DynamoDBv2
+﻿open Amazon.DynamoDBv2
 open Amazon.DynamoDBv2.DocumentModel
 open Amazon.DynamoDBv2.Model
 open Amazon.Runtime
-open DynamoDb
+open RealtyShares
+open RealtyShares.DynamoDb
+open RealtyShares.DynamoEventStore
 
 [<EntryPoint>]
 let main argv = 
-    let tableName = "tracy-test"
+    // Use credentials from environmental variables, and "N. California",
+    // which is `Amazon.RegionEndpoint.USWest1`, is our default. -- D.K.
+    let credentials = new EnvironmentVariablesAWSCredentials()
+    let client = new AmazonDynamoDBClient(credentials, Amazon.RegionEndpoint.USWest1)
+    let tableName = "StandaloneFSharp-demo-table"
+
     async {
         printfn "Creating table, %s..." tableName
-        let! res = DynamoDb.createTable tableName
-        let! status = DynamoDb.waitForStatus tableName TableStatus.ACTIVE
+        let! res = createTable client tableName
+        let! status = waitForStatus client tableName TableStatus.ACTIVE
         printfn "Table created!"
 
-        let t = Table.LoadTable(DynamoDb.client, tableName)
+        let table = Table.LoadTable(client, tableName)
 
-        let save (table:Table) id version body = 
-            table.PutItem(dict [
-                "H", id
-                "K", version
-                "V", body
-            ])
+        let sw = new System.Diagnostics.Stopwatch()
+        sw.Start()
+    
+        printfn "inserting %d entries..." 1000
+        try 
+            async {
+                let inserts = 
+                    [0..1000]
+                    |> List.mapi (fun index version -> save table index version (sprintf "version %d" version))
+                for entry in inserts do 
+                    let! _ = entry
+                    ()
+            } |> Async.RunSynchronously |> ignore
+        with e -> printfn "error: %s" (string e)
 
-        let _ =    
-            [0..10]
-            |> List.map (fun version -> save t 0 version (sprintf "version %d" version)
+        sw.Stop()
 
-        do! Asyncleep 2000
+        let writeSpeed = 1000.0 / sw.Elapsed.TotalMilliseconds * 1000.00
+        printfn "wrote %d entries at %f writes/sec" 1000 writeSpeed
+
+        let sw2 = new System.Diagnostics.Stopwatch()
+        sw2.Start()
+    
+        load table "0"  
+        |> Async.RunSynchronously
+        |> (fun events -> printfn "read %d events" events.Length)
+
+        sw2.Stop()
+
+        let readSpeed = 1000.0 / sw2.Elapsed.TotalMilliseconds * 1000.00
+        printfn "read %d entries at %f reads/sec" 1000 readSpeed
 
         printfn "Deleting table, %s..." tableName
-        let! del = DynamoDb.deleteTable tableName
+        let! del = deleteTable client tableName
         //let! status = DynamoDb.waitForStatus tableName TableStatus.
         
         printfn "Table deleted!"
--- RESTfulService/FSharp/FSharp/Program.fs
+++ RESTfulService/FSharp/FSharp/Program.fs
@@ -247,34 +247,34 @@ let spawn_server routes =
     
 // Unit tests.
 [<TestFixture>]
-type UnitTest() =
+module UnitTest =
     // Test setup.
     [<TestFixtureSetUp>]
-    member this.``Bring up the server``() = ignore <| spawn_server app
+    let ``Bring up the server``() = spawn_server app
 
     // Demo endpoints test.
     [<Test>]
-    member this.``DEMO: GET /demo_get``() =
+    let ``DEMO: GET /demo_get``() =
         "OK"
         |> should equal
         <| Http.RequestString(sprintf "http://localhost:%d/demo_get" port)
     [<Test>]
     [<ExpectedException>]
-    member this.``DEMO: POST /demo_get not allowed``() =
+    let ``DEMO: POST /demo_get not allowed``() =
         should throw typeof<System.Net.WebException>
         <| Http.RequestString((sprintf "http://localhost:%d/demo_get" port), httpMethod="POST")
     [<Test>]
-    member this.``DEMO: GET /demo_add/2/2``() =
+    let ``DEMO: GET /demo_add/2/2``() =
         "2+2=4"
         |> should equal
         <| Http.RequestString(sprintf "http://localhost:%d/demo_add/2/2" port)
     [<Test>]
-    member this.``DEMO: GET /demo_whatever?demo_url_parameter=foo"``() =
+    let ``DEMO: GET /demo_whatever?demo_url_parameter=foo"``() =
         "demo_url_parameter=foo"
         |> should equal
         <| Http.RequestString(sprintf "http://localhost:%d/demo_whatever?demo_url_parameter=foo" port)
     [<Test>]
-    member this.``DEMO: POST /demo_post``() =
+    let ``DEMO: POST /demo_post``() =
         "LENGTH=3, BODY='foo'"
         |> should equal
         <| Http.RequestString((sprintf "http://localhost:%d/demo_post" port),
@@ -283,36 +283,36 @@ type UnitTest() =
 
     // API endpoints test.
     [<Test>]
-    member this.``API: GET /add``() =
+    let ``API: GET /add``() =
         """{"sum":5}"""
         |> should equal
         <| Http.RequestString(sprintf "http://localhost:%d/add?a=2&b=3" port)
     [<Test>]
-    member this.``API: GET /subtract``() =
+    let ``API: GET /subtract``() =
         """{"result":4}"""
         |> should equal
         <| Http.RequestString(sprintf "http://localhost:%d/subtract?a=5&b=1" port)
     [<Test>]
-    member this.``API: GET /concatenate``() =
+    let ``API: GET /concatenate``() =
         """{"concatenated":"foo bar"}"""
         |> should equal
         <| Http.RequestString(sprintf "http://localhost:%d/concatenate?p=foo%s&q=bar" port "%20")
     [<Test>]
-    member this.``API: POST /multiply``() =
+    let ``API: POST /multiply``() =
         """{"product":35}"""
         |> should equal
         <| Http.RequestString((sprintf "http://localhost:%d/multiply" port),
                               httpMethod = "POST",
                               body = TextRequest """{"x":5,"y":7}""")
     [<Test>]
-    member this.``API: POST /divide``() =
+    let ``API: POST /divide``() =
         """{"result":1.25}"""
         |> should equal
         <| Http.RequestString((sprintf "http://localhost:%d/divide" port),
                               httpMethod = "POST",
                               body = TextRequest """{"x":5,"y":4}""")
     [<Test>]
-    member this.``API: POST /divide with invalid data``() =
+    let ``API: POST /divide with invalid data``() =
         """{"error":"Parameter 'y' should not be zero."}"""
         |> should equal
         <| Http.RequestString((sprintf "http://localhost:%d/divide" port),
@@ -320,14 +320,14 @@ type UnitTest() =
                               body = TextRequest """{"x":100,"n":0}""",
                               silentHttpErrors = true)
     [<Test>]
-    member this.``API: POST /repeat``() =
+    let ``API: POST /repeat``() =
         """{"result":"foofoofoo"}"""
         |> should equal
         <| Http.RequestString((sprintf "http://localhost:%d/repeat" port),
                               httpMethod = "POST",
                               body = TextRequest """{"s":"foo","n":3}""")
     [<Test>]
-    member this.``API: POST /repeat with invalid data``() =
+    let ``API: POST /repeat with invalid data``() =
         """{"error":"Parameter 'n' should be positive."}"""
         |> should equal
         <| Http.RequestString((sprintf "http://localhost:%d/repeat" port),
