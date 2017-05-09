09486e3cba6feb0380897391d2f5828ef2a00204
Dima <dmitry.korolev@gmail.com>
2017-05-09 12:59:04 -0700

    ~TypeScript `enum`-s FTW!

--- TypeScript/du.ts
+++ TypeScript/du.ts
@@ -6,19 +6,19 @@ Like { key: { lid: 3001 }, user: { uid: 1001 }, post: { pid: 2001 } }
 User 'Dima' liked post 'Strong typing FTW!'
 */
 
-interface UserID { uid: number; }
-interface PostID { pid: number; }
-interface LikeID { lid: number; }
+enum UserID { INVALID_UID = 0 }
+enum PostID { INVALID_PID = 0 }
+enum LikeID { INVALID_LID = 0 }
 
 class DBContents {
-  user_name: object = {};
-  post_text: object = {};
+  user_name: Map<UserID, string> = new Map<UserID, string>();
+  post_text: Map<PostID, string> = new Map<PostID, string>();
 }
 
 class User {
   public constructor(public key: UserID, public name: string) {}
   updateDB(db: DBContents) {
-    db.user_name[this.key.uid] = this.name;
+    db.user_name[this.key] = this.name;
   }
   dumpGivenDBContents(db: DBContents) {}
 }
@@ -26,7 +26,7 @@ class User {
 class Post {
   public constructor(public key: PostID, public text: string) {}
   updateDB(db: DBContents) {
-    db.post_text[this.key.pid] = this.text;
+    db.post_text[this.key] = this.text;
   }
   dumpGivenDBContents(db: DBContents) {}
 }
@@ -35,16 +35,16 @@ class Like {
   public constructor(public key: LikeID, public user: UserID, public post : PostID) {}
   updateDB(db: DBContents) {}
   dumpGivenDBContents(db: DBContents) {
-    console.log(`User '${db.user_name[this.user.uid]}' liked post '${db.post_text[this.post.pid]}'`);
+    console.log(`User '${db.user_name[this.user]}' liked post '${db.post_text[this.post]}'`);
   }
 }
 
 type Entry = User | Post | Like;
 
 let data: Entry[] = [
-  new User({ uid: 1001 }, "Dima"),
-  new Post({ pid: 2001 }, "Strong typing FTW!"),
-  new Like({ lid: 3001 }, { uid: 1001 }, { pid: 2001 }),
+  new User(<UserID>1001, "Dima"),
+  new Post(<PostID>2001, "Strong typing FTW!"),
+  new Like(<LikeID>3001, <UserID>1001, <PostID>2001),
 ];
 
 data.forEach((e: Entry) => {
